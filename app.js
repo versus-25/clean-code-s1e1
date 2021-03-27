8df4a8c9f1fafdbc3fb225c60b844d8fd565497a
versus-25 <ivan_schkola@mail.ru>
2021-03-27 19:35:11 +0300

    refactor: replace selector tag to selector class in text of task 3.1,3.2,3.3,3.4 and 2.1

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var createNewTaskElement = function (taskString) {
   var deleteButtonImg = document.createElement('img'); //delete button image
 
   label.innerText = taskString;
-  label.className = 'task';
+  label.className = 'task-block__text';
 
   //Each elements, needs appending
   checkBox.type = 'checkbox';
